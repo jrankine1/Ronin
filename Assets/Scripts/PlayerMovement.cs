5967919aee581aeecba46db676a1a29f7da5be6c
jrankine1 <85931070+jrankine1@users.noreply.github.com>
2022-04-20 21:58:43 +1000

    Player Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Video Tutorial: https://www.youtube.com/watch?v=9HAZQROH2gM
+
+    public float moveSpeed;
+    private Rigidbody2D rb;
+    private bool facingRight = true;
+    private float moveDirection;
+    public float jumpForce;
+    private bool isjumping = false;
+    public Transform groundCheck;
+    public LayerMask groundObjects;
+    private bool isGrounded;
+    public float checkRadius;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = Input.GetAxis("Horizontal");
+
+
+        rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+
+        
+
+        if(moveDirection > 0 && !facingRight)
+        {
+            FlipCharacter();
+        }
+        else if(moveDirection < 0 && facingRight)
+        {
+            FlipCharacter();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Jump();
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundObjects);
+    }
+
+    private void FlipCharacter()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0f, 180f, 0f);
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            isjumping = true;
+        }
+
+
+        if (isjumping)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+        }
+        isjumping = false;
+    }
+}
